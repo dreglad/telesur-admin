b2df36c953bdff25170a15b0c046a75722a36f00
David Regla <dreglad@gmail.com>
2018-11-27 22:12:25 -0600

    :globe_with_meridians: Add i18n to admin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 // in App.js
 import React, { Component } from 'react';
 import { Admin, Resource, Delete, ListGuesser, EditGuesser } from 'react-admin';
-
 import { ArticleList, ArticleSectionList } from './components/articles';
 import { ServiceList } from './components/services';
-
 import buildProvider from './dataProvider';
+import englishMessages from 'ra-language-english';
+import spanishMessages from 'ra-language-spanish';
+
+const messages = {
+  en: englishMessages,
+  es: spanishMessages
+}
+
+const i18nProvider = locale => {
+  console.log(messages[locale])
+  return messages[locale];
+};
 
 class App extends Component {
   constructor() {
@@ -25,7 +35,7 @@ class App extends Component {
     }
 
     return (
-      <Admin dataProvider={dataProvider}>
+      <Admin dataProvider={dataProvider} locale="es" i18nProvider={i18nProvider}>
         <Resource name="Service" list={ServiceList}/>
         <Resource name="ArticleSection" list={ArticleSectionList}/>
         <Resource name="Article" list={ArticleList}/>
